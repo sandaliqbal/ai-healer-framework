069d56b753064bdc9b37b5ca2d702b30adb0a3fb
sandaliqbal <sandaliqbal@yahoo.com>
2026-01-12 13:18:53 +0530

    some fix

--- adapter/selfheal/locator_proxy.py
+++ adapter/selfheal/locator_proxy.py
@@ -9,13 +9,6 @@ class HealingLocatorProxy:
         self._healer = healer
 
     def click(self, **kwargs):
-        self._page.get_by_alt_text
-        self._page.get_by_label
-        self._page.get_by_placeholder
-        self._page.get_by_role
-        self._page.get_by_test_id
-        self._page.get_by_text
-        self._page.get_by_title
         return self._execute("click", **kwargs)
 
     def fill(self, value, **kwargs):
